528175b772612082fd1852dc3498b60f44b6698c
Grintona <misha2007gg23@gmail.com>
2025-12-16 19:22:00 +0500

    start

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 class Main {
     public static void main(String[] args) {
-    Rectangle rectangle = new Rectangle(12, 12);
-    System.out.println(rectangle);
+        int flag = 0;
+    while(flag != 7){
 
-    System.out.println("***************************************************");
 
-    Book b = new Book();
-    b.setTitle("War and Peace");
-    b.setAuthor("Tolstoy");
-    b.setAvailable(true);
-    b.setYear(1867);
-    System.out.println(b.getAuthor());
-    b.markAsBorrowed();
-    System.out.println(b.getAuthor());
-    System.out.println(b);
+
+
+    }
     }
 }
