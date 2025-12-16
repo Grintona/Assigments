060e702e8042b37c5fa2382972546810878560fc
Grintona <misha2007gg23@gmail.com>
2025-12-16 18:50:34 +0500

    Second task is done

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,16 @@ class Main {
     Rectangle rectangle = new Rectangle(12, 12);
     System.out.println(rectangle);
 
+    System.out.println("***************************************************");
+
+    Book b = new Book();
+    b.setTitle("War and Peace");
+    b.setAuthor("Tolstoy");
+    b.setAvailable(true);
+    b.setYear(1867);
+    System.out.println(b.getAuthor());
+    b.markAsBorrowed();
+    System.out.println(b.getAuthor());
+    System.out.println(b);
     }
 }
